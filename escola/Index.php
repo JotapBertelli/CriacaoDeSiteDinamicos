75be5f3c0037543e2e0e9abd12eabdacc9311d4b
João Pedro da Silva Bertelli <109303186+JotapBertelli@users.noreply.github.com>
2026-04-02 22:00:57 -0300

    Add files via upload

---
+++ escola/Index.php
@@ -0,0 +1,30 @@
+<?php
+
+$hostname = "localhost";
+$username = "fulano";
+$password = "123456";
+$usertable = "aluno";
+$key = $_POST['id'];
+
+$conn=mysqli_connect($hostname,$username,$password)
+    or die("Unable to connect to database!");
+mysqli_select_db($conn,$dbname)
+    or die("Unable to select dbname");
+echo "<h1> Dados do aluno com ID = $key </h1>";
+
+
+$query = "SELECT * FROM $usertable where id =$key";
+
+$result = mysqli_query($conn,$query);
+
+if ($result) {
+    while($row = mysqli_fetch_array($result)){
+        $name = $row['nome'];
+        $email = $row['email'];
+        $nasc = $row['nascimento'];
+        echo "Nome: " . $name  .   "<br>Nascimento:"  . $nasc . "<br>Email:" . $email . "  <br>";
+
+
+    }
+}
+else echo "Not Users founds in this table";
