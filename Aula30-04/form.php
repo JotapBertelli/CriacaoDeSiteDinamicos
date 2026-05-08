25c592b68f3d2aa8827ab43ba6f2ec69bef70588
João Pedro da Silva Bertelli <109303186+JotapBertelli@users.noreply.github.com>
2026-05-07 22:07:02 -0300

    Update form.php

--- Aula30-04/form.php
+++ Aula30-04/form.php
@@ -7,7 +7,15 @@
     $usuario = $query_result->fetch(PDO::FETCH_OBJ);
     echo "$usuario->nome <br>";
     echo '$usuario->endereco <br>';
+    
 
-
+    $query = "select  placa,tipo,modelo from veiculo where cpf = '$key'";
+    $query_result = $pdo->query($query);
+    $veics = $query_result->fetch(PDO::FETCH_OBJ);
+    echo "<table border>;
+    foreach($veics as $veic){
+    $veiculo->display();
+}
+    echo "</table>";
 
 ?>
